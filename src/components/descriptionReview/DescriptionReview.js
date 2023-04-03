3e22a75fed73ca1cee0c7c078d6b6e2afa1ab284
ah-sultan <ah.sultan.dev@gmail.com>
2023-04-03 16:07:42 +0600

    tab

--- src/components/descriptionReview/DescriptionReview.js
+++ src/components/descriptionReview/DescriptionReview.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ProductInformation from './ProductInformation'
 import ProductDescription from './ProductDescription'
@@ -6,28 +6,31 @@ import ProductReviews from './ProductReviews'
 
 
 function DescriptionReview() {
-  return (
-   <section className="pb-100px">
-    <div className="container">
-        <div className="h-60px flex justify-center rounded-[5px] bg-light-700 gap-x-2.5">
-            <button className="text-lg leading-6 bg-transparent text-dark-400 font-semibold trns-1 py-18px px-5 hover:text-white hover:bg-primary-900">Information</button>
-            <button className="text-lg leading-6 bg-transparent text-dark-400 font-semibold trns-1 py-18px px-5 hover:text-white hover:bg-primary-900">Description</button>
-            <button className="text-lg leading-6 bg-transparent text-dark-400 font-semibold trns-1 py-18px px-5 hover:text-white hover:bg-primary-900">Reviews (02)</button>
-        </div>
-        <div className="pt-60px">
-            <div className="hidden">
-                <ProductInformation/>
+    const [active, setActive] = useState('')
+    console.log(active)
+
+    return (
+        <section className="pb-100px">
+            <div className="container">
+                <div className="h-60px flex justify-center rounded-[5px] bg-light-700 gap-x-2.5">
+                    <button className="text-lg leading-6 bg-transparent text-dark-400 font-semibold trns-1 py-18px px-5 hover:text-white hover:bg-primary-900" onClick={() => setActive('information')}>Information</button>
+                    <button className="text-lg leading-6 bg-transparent text-dark-400 font-semibold trns-1 py-18px px-5 hover:text-white hover:bg-primary-900" onClick={() => setActive('description')}>Description</button>
+                    <button className="text-lg leading-6 bg-transparent text-dark-400 font-semibold trns-1 py-18px px-5 hover:text-white hover:bg-primary-900" onClick={() => setActive('reviews')}>Reviews (02)</button>
+                </div>
+                <div className="pt-60px overflow-hidden">
+                    <div className={`opacity-0 invisible trns-1 overflow-hidden ${active === 'information' ? '!visible !opacity-100' : undefined}`}>
+                        <ProductInformation />
+                    </div>
+                    <div className={`opacity-0 invisible trns-1 overflow-hidden ${active === 'description' ? '!visible !opacity-100' : undefined}`}>
+                        <ProductDescription />
+                    </div>
+                    <div className={`opacity-0 invisible trns-1 overflow-hidden ${active === 'reviews' ? '!visible !opacity-100' : undefined}`}>
+                        <ProductReviews />
+                    </div>
+                </div>
             </div>
-            <div className="hidden">
-                <ProductDescription/>
-            </div>
-            <div className="block">
-                <ProductReviews/>
-            </div>
-        </div>
-    </div>
-   </section>
-  )
+        </section>
+    )
 }
 
 export default DescriptionReview
--- src/components/descriptionReview/ProductReviews.js
+++ src/components/descriptionReview/ProductReviews.js
@@ -1,39 +1,39 @@
 import Image from 'next/image'
-import {FaStar} from 'react-icons/fa'
+import { FaStar } from 'react-icons/fa'
 
 // Images
 import img1 from '../../../public/img/review/1.png'
 import img2 from '../../../public/img/review/2.png'
 
-function SingleReview(props){
-    return(
+function SingleReview(props) {
+    return (
         <>
             <div className="w-full flex items-center mb-50px">
                 {/* Image Section */}
                 <div className="mr-15px min-w-[120px]">
-                    <Image src={props.img} alt='img1'/>
+                    <Image src={props.img} alt='img1' />
                 </div>
                 <div>
-                   <div className="flex items-center justify-between">
-                    {/* Name & Review Sectio */}
+                    <div className="flex items-center justify-between">
+                        {/* Name & Review Sectio */}
                         <div className="flex items-center">
                             <h5 className="mr-15px text-xl leading-none text-[#525252] font-semibold mb-0">{props.name}</h5>
                             <div className="items-center flex">
-                                <FaStar className="inline-block text-accent-900 text-sm leading-none"/>
-                                <FaStar className="inline-block text-accent-900 text-sm leading-none"/>
-                                <FaStar className="inline-block text-accent-900 text-sm leading-none"/>
-                                <FaStar className="inline-block text-accent-900 text-sm leading-none"/>
-                                <FaStar className="inline-block text-accent-900 text-sm leading-none"/>
+                                <FaStar className="inline-block text-accent-900 text-sm leading-none" />
+                                <FaStar className="inline-block text-accent-900 text-sm leading-none" />
+                                <FaStar className="inline-block text-accent-900 text-sm leading-none" />
+                                <FaStar className="inline-block text-accent-900 text-sm leading-none" />
+                                <FaStar className="inline-block text-accent-900 text-sm leading-none" />
                             </div>
                         </div>
                         <button className="text-base font-normal leading-relaxed text-[#525252] hover:text-primary-900">
                             Reply
                         </button>
-                   </div>
-                   {/* Text */}
-                   <div>
-                    <p className="w-[93%] text-base font-normal leading-relaxed text-tGreay-300 mt-10px">{props.text}</p>
-                   </div>
+                    </div>
+                    {/* Text */}
+                    <div>
+                        <p className="w-[93%] text-base font-normal leading-relaxed text-tGreay-300 mt-10px">{props.text}</p>
+                    </div>
                 </div>
             </div>
         </>
@@ -43,25 +43,47 @@ function SingleReview(props){
 
 
 function ProductReviews() {
-  return (
-    <>
-        <section>
-            <div className="container px-0">
-                <div className="flex">
-                    <div className="w-7/12 gap-x-6">
-                        <div>
-                            <SingleReview img={img1} name="White Lewis" text="Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae Suspendisse viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam nisi, congue id nulla. "/>
-                            <div className="pl-70px">
-                                <SingleReview img={img2} name="White Lewis" text="Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae Suspendisse viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam nisi, congue id nulla. "/>
+    return (
+        <>
+            <section>
+                <div className="container px-0">
+                    <div className="flex">
+                        <div className="w-7/12 gap-x-6">
+                            <div>
+                                <SingleReview img={img1} name="White Lewis" text="Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae Suspendisse viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam nisi, congue id nulla. " />
+                                <div className="pl-70px">
+                                    <SingleReview img={img2} name="White Lewis" text="Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae Suspendisse viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam nisi, congue id nulla. " />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-5/12">
+                            <div className="pl-50px">
+                                <h6 className="text-lg leading-1.2 font-semibold text-dark-300">Add a Review</h6>
+                                {/* Form Section */}
+                                <form action="#">
+                                    {/* Rating */}
+                                    <div className="mt-2.5 mb-5">
+                                        <span className="text-lg leading-none font-semibold text-dark-300 inline-block mr-15px">Your rating:</span>
+                                        <FaStar className="inline-block text-accent-900 text-sm leading-none" />
+                                        <FaStar className="inline-block text-accent-900 text-sm leading-none" />
+                                        <FaStar className="inline-block text-accent-900 text-sm leading-none" />
+                                        <FaStar className="inline-block text-accent-900 text-sm leading-none" />
+                                        <FaStar className="inline-block text-accent-900 text-sm leading-none" />
+                                    </div>
+                                    <div className="flex gap-x-6">
+                                        <input type="text" placeholder="Name" className="h-[45px] w-1/2 text-[#474747] text-sm leading-1.2 py-0.5 pl-5 pr-2.5 border border-[#ebebeb] mb-15px" />
+                                        <input type="email" placeholder="Email" className="h-[45px] w-1/2 text-[#474747] text-sm leading-1.2 py-0.5 pl-5 pr-2.5 border border-[#ebebeb] mb-15px" />
+                                    </div>
+                                    <textarea placeholder="Message" className="h-[213px] w-full text-[#474747] text-sm leading-1.2 py-5 pl-5 pr-2.5 border border-[#ebebeb] mb-5" />
+                                    <button type="submit" className="btn-primary w-[170px] h-50px font-bold text-base leading-none">Submit</button>
+                                </form>
                             </div>
                         </div>
                     </div>
-                    <div className="w-5/12"></div>
                 </div>
-            </div>
-        </section>
-    </>
-  )
+            </section>
+        </>
+    )
 }
 
 export default ProductReviews
