bfec38543ed7c634e078956703a441d1ff8d5b34
ah-sultan <ah.sultan.dev@gmail.com>
2023-04-06 15:30:55 +0600

    fetch product from api

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,40 +1,44 @@
 import ProductCard from "../productCard/ProductCard"
-import {BiRightArrowAlt} from 'react-icons/bi'
-
-// images 
-import productImg from '../../../public/img/products/1.jpg'
+import { BiRightArrowAlt } from 'react-icons/bi'
 
 
-function Products() {
-  return (
-    <>
-    <section className="py-100px">
-        <div className="container">
-            <div className="text-center">
-                <h2 className="section-title">#product</h2>
-            </div>
-            <div className="flex items-center justify-center gap-x-2.5 mt-25px mb-60px">
-                <button className="product-tab-btn active">All</button>
-                <button className="product-tab-btn active">New</button>
-                <button className="product-tab-btn active">Man</button>
-                <button className="product-tab-btn active">Women</button>
-                <button className="product-tab-btn active">KIds</button>
-            </div>
-            <div>
-                <div className="flex gap-x-6 px-3">
-                    <ProductCard img={productImg} discount="-10%" new="new" review="5" title="Women's Elizabeth Coat" price="60.65" discountPrice="56.70"/>
-                    <ProductCard img={productImg} discount="-10%" new="new" review="5" title="Women's Elizabeth Coat" price="60.65" discountPrice="56.70"/>
-                    <ProductCard img={productImg} discount="-10%" new="new" review="5" title="Women's Elizabeth Coat" price="60.65" discountPrice="56.70"/>
-                    <ProductCard img={productImg} discount="-10%" new="new" review="5" title="Women's Elizabeth Coat" price="60.65" discountPrice="56.70"/>
+
+function Products(props) {
+
+    const product = props.product.slice(0, 8);
+
+    return (
+        <>
+            <section className="py-100px">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-title">#product</h2>
+                    </div>
+                    <div className="flex items-center justify-center gap-x-2.5 mt-25px mb-60px">
+                        <button className="product-tab-btn active">All</button>
+                        <button className="product-tab-btn active">New</button>
+                        <button className="product-tab-btn active">Man</button>
+                        <button className="product-tab-btn active">Women</button>
+                        <button className="product-tab-btn active">KIds</button>
+                    </div>
+                    <div>
+                        <div className="grid grid-cols-4 gap-30px">
+                            {
+                                product.map((product) => {
+                                    const { id, brand, rating, title, reviews, thumbnail, price, description, category, sku } = product
+
+                                    return <ProductCard key={id} id={id} brand={brand} title={title} rating={rating} reviews={reviews} thumbnail={thumbnail} price={price} description={description} category={category} sku={sku} />
+                                })
+                            }
+                        </div>
+                    </div>
+                    <div className="text-center mt-60px">
+                        <button className="btn-primary  w-[210px] h-[65px] font-bold center-child mx-auto gap-x-15px">Load More <BiRightArrowAlt className="inline-block text-xl" /></button>
+                    </div>
                 </div>
-            </div>
-            <div className="text-center mt-60px">
-                <button className="btn-primary  w-[210px] h-[65px] font-bold center-child mx-auto gap-x-15px">Load More <BiRightArrowAlt className="inline-block text-xl"/></button>
-            </div>
-        </div>
-    </section>
-    </>
-  )
+            </section>
+        </>
+    )
 }
 
 export default Products
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,22 +10,37 @@ import Footer from '@/components/footer/Footer'
 import QuickView from '@/components/quickView/QuickView'
 import THead from '@/components/thead/THead'
 
-export default function Home() {
+// https://api.npoint.io/c622a81c9c8ae72fa0ef
+
+export default function Home(props) {
+
   return (
+
     <>
-      
+
       <main>
-        <THead/>
+        <THead />
         <Header />
         <Hero />
-        <Feature/>
-        <Products/>
-        <Banner/>
-        <Newarrivals/>
-        <Deal/>
-        <Blog/>
-        <Footer/>
+        <Feature />
+        <Products product={props.productData} />
+        <Banner />
+        <Newarrivals product={props.productData} />
+        <Deal />
+        <Blog />
+        <Footer />
       </main>
     </>
   )
 }
+
+export async function getServerSideProps(context) {
+
+  const res = await fetch('https://api.npoint.io/fbae6ac9be2f693e0e03')
+  const productData = await res.json()
+
+  return {
+    props: { productData }
+  }
+}
+
