0b789032f412c5a46128c3da91307c64af508ea9
ah-sultan <ah.sultan.dev@gmail.com>
2023-04-09 14:20:36 +0600

    Created Mega Menu

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,8 +9,8 @@ import { FaPhone, FaEnvelope, FaLocationArrow, FaArrowRight } from "react-icons/
 import { CiLocationOn } from "react-icons/ci"
 
 
-function ContactCard(props){
-    return(
+function ContactCard(props) {
+    return (
         <>
             <div className={`border flex items-center px-35px py-30px ${props.className} group`}>
                 <div className="h-70px w-70px rounded-full center-child gradient-primary ">
@@ -28,51 +28,51 @@ function ContactCard(props){
 }
 
 function Contact() {
-  return (
-    <>
-        <THead/>
-        <Header/>
-        <main>
-            <Breadcrumb title="Contact Us" pages="home"/>
-            <section className="py-100px">
-                <div className="container">
-                    <div className="p-70px bg-white t-shadow-7">
-                        <div className="flex gap-x-6">
-                            <div className="w-5/12">
-                                <ContactCard icon={<FaPhone/>} action="tel:012 345 67 89" title="Phone:" text="012 345 67 89" className=""/>
-                                <ContactCard icon={<FaEnvelope/>} action="mailto:demo@example.com" title="Email:" text="demo@example.com" className="mt-30px"/>
-                                <ContactCard icon={<FaLocationArrow/>} action="#" title="Address:" text="Your address goes here" className="mt-30px"/>
-                            </div>
-                            <div className="w-7/12">
-                                <h4 className="text-black text-30px leading-1.2 mb-12.5">Leave a Message</h4>
-                                <p className="text-base leading-7 text-dark-700">There are many variations of passages of Lorem Ipsum available but the suffered alteration in some form.</p>
-                                <div className="mt-30px">
-                                    <form action="#">
-                                        <div className="grid grid-cols-2 gap-6">
-                                            <div>
-                                                <input type="text" className="form-input" placeholder="Name *"/>
+    return (
+        <>
+            <THead />
+            <Header />
+            <main>
+                <Breadcrumb title="Contact Us" pages="home" />
+                <section className="py-100px">
+                    <div className="container">
+                        <div className="p-70px bg-white t-shadow-7">
+                            <div className="flex gap-x-6">
+                                <div className="w-5/12">
+                                    <ContactCard icon={<FaPhone />} action="tel:012 345 67 89" title="Phone:" text="012 345 67 89" className="" />
+                                    <ContactCard icon={<FaEnvelope />} action="mailto:demo@example.com" title="Email:" text="demo@example.com" className="mt-30px" />
+                                    <ContactCard icon={<FaLocationArrow />} action="#" title="Address:" text="Your address goes here" className="mt-30px" />
+                                </div>
+                                <div className="w-7/12 pl-30px">
+                                    <h4 className="text-black text-30px leading-1.2 mb-12.5 font-semibold">Leave a Message</h4>
+                                    <p className="text-base leading-7 text-dark-700">There are many variations of passages of Lorem Ipsum available but the suffered alteration in some form.</p>
+                                    <div className="mt-30px">
+                                        <form action="#">
+                                            <div className="grid grid-cols-2 gap-6">
+                                                <div>
+                                                    <input type="text" className="form-input" placeholder="Name *" />
+                                                </div>
+                                                <div>
+                                                    <input type="Email" className="form-input" placeholder="Email *" />
+                                                </div>
                                             </div>
-                                            <div>
-                                                <input type="Email" className="form-input" placeholder="Email *"/>
+                                            <div className="pt-30px">
+                                                <textarea placeholder="Your Message *" className="form-input h-[200px] focus:outline-none py-6 leading-normal" />
                                             </div>
-                                        </div>
-                                        <div className="pt-30px">
-                                            <textarea placeholder="Your Message *" className="form-input h-[200px] focus:outline-none py-6 leading-normal"/>
-                                        </div>
-                                        <button className="btn-primary center-child text-sm leading-none w-[210px] h-[65px] mt-30px tracking-wide font-semibold">Post Comment <FaArrowRight className="ml-1"/> </button>
-                                    </form>
+                                            <button className="btn-primary center-child text-sm leading-none w-[210px] h-[65px] mt-30px tracking-wide font-semibold">Post Comment <FaArrowRight className="ml-1" /> </button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
-                        
                     </div>
-                </div>
-            </section>
-        </main>
-        <Footer/>
-    
-    </>
-  )
+                </section>
+            </main>
+            <Footer />
+
+        </>
+    )
 }
 
 export default Contact
