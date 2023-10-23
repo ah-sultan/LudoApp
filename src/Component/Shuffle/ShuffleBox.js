e8f06c85604802886c22a746f72a2a4878f2cfd9
sultan-office <web.sultan54@gmail.com>
2023-10-23 17:24:02 +0600

    done

--- src/Component/Shuffle/ShuffleBox.js
+++ src/Component/Shuffle/ShuffleBox.js
@@ -10,63 +10,63 @@ const ShuffleBox = ({
   const [number, setNumber] = useState(1)
   const [count, setCount] = useState(0)
   const [track, setTrack] = useState([])
-  
+
 
 
   const dispatch = useDispatch()
 
-  // const ShuffleHandler = () => {
-  //   const randomNumber = Math.ceil(Math.random() * 6)
-  //   setNumber(0)
+  const ShuffleHandler = () => {
+    const randomNumber = Math.ceil(Math.random() * 6)
+    setNumber(0)
 
-  //    setTimeout(() => {
-  //     setNumber(randomNumber)
-  //     setTrack([...track, randomNumber])
+    setTimeout(() => {
+      setNumber(randomNumber)
+      setTrack([...track, randomNumber])
+      dispatch(shuffleAction({
+        currentValue: randomNumber,
+        dicesValue: randomNumber,
+        playerName: playerName,
+      }))
+
+    }, 100)
+
+
+  }
+
+  console.log(track)
+
+  // ----------------------- Draft _________________________________
+
+  // const arr = [6, 6, 5, 5, 6, 6, 5, 2, 6, 4, 3,]
+  // const ShuffleHandler = () => {
+
+  //   setCount(count + 1)
+
+  //   setTimeout(() => {
+  //     setNumber(arr[count])
   //     dispatch(shuffleAction({
-  //       currentValue : randomNumber,
-  //       dicesValue : randomNumber,
-  //       playerName : playerName,
+  //       currentValue: arr[count],
+  //       dicesValue: arr[count],
+  //       playerName: playerName,
   //     }))
 
-  //    }, 100)
-
+  //   }, 200)
 
   // }
 
-  // console.log(track)
-
-  // ----------------------- Draft _________________________________
- 
-const arr = [6,6,5,6,6,5,6,6,5,6,6,5,6,6,5,6,1,6,2,6,3,6,4,6,5,6,1,1,2,6,1,6,2,6,3,6,4,6,5,6,1,1,2]
-  const ShuffleHandler = () => {
-    
-    setCount(count + 1)
-    
-     setTimeout(() => {
-      setNumber(arr[count])
-      dispatch(shuffleAction({
-        currentValue : arr[count],
-        dicesValue :  arr[count],
-        playerName : playerName,
-      }))
-
-     }, 200)
-   
-  }
-
   return (
     <>
-    <div onClick={() => ShuffleHandler()} className="h-14 w-14 rounded flex justify-center items-center border">
-      <ul className="p-0.5 grid grid-cols-[auto_auto]  justify-center items-center gap-1.5 ">
+      <div onClick={() => ShuffleHandler()} className="h-14 w-14 rounded flex justify-center items-center border">
+        <ul className="p-0.5 grid grid-cols-[auto_auto]  justify-center items-center gap-1.5 ">
           {
-            Array(number).fill().map((_,index) => {
-              return(
+            Array(number).fill().map((_, index) => {
+              return (
                 <li key={index} className="w-1.5 h-1.5 rounded-full bg-black animate-shuffleRotate"></li>
               )
             })
           }
-      </ul>
-    </div>
+        </ul>
+      </div>
     </>
   )
 }
--- src/feature/CellType.js
+++ src/feature/CellType.js
@@ -1,49 +1,49 @@
 import { FinisherCellName, MainCellName, SuccessCellName, player1, player2, player3, player4 } from "@/Data/Data"
 
-export const diceList = Array(4).fill().map((_,index) => {
+export const diceList = Array(4).fill().map((_, index) => {
   const cell = {
     id: index + 1,
-    playerName : null,
+    playerName: null,
     dicesValue: [],
-    previousValue :0,
-    currentValue :0,
-    readyAction : false,
-    currentCell : null,
-    inHouse : true,
-    inMainCell : false,
-    inFinisherCell : false,
-    inSuccessCell : false,
+    previousValue: 0,
+    currentValue: 0,
+    readyAction: false,
+    currentCell: null,
+    inHouse: true,
+    inMainCell: false,
+    inFinisherCell: false,
+    inSuccessCell: false,
   }
 
   return cell
 })
 
-export const mainCells = Array(52).fill().map((_,index) => {
-    const cell = {
-      id: index + 1,
-      cellName : MainCellName,
-      activePlayer : null
-    }
+export const mainCells = Array(52).fill().map((_, index) => {
+  const cell = {
+    id: index + 1,
+    cellName: MainCellName,
+    activePlayer: null
+  }
 
-    return cell
-  } )
-
-
-export const finisherCell = Array(5).fill().map((_,index) => {
-    const cell = {
-      id: index + 1,
-      cellName : FinisherCellName,
-      dices: ''
-    }
-
-    return cell
+  return cell
 })
 
 
-export const successCell = Array(4).fill().map((_,index) => {
+export const finisherCell = Array(5).fill().map((_, index) => {
   const cell = {
     id: index + 1,
-    cellName : SuccessCellName,
+    cellName: FinisherCellName,
+    dices: ''
+  }
+
+  return cell
+})
+
+
+export const successCell = Array(4).fill().map((_, index) => {
+  const cell = {
+    id: index + 1,
+    cellName: SuccessCellName,
     dices: ''
   }
 
@@ -53,3 +53,60 @@ export const successCell = Array(4).fill().map((_,index) => {
 
 
 
+export const testDices = [
+  {
+    id: 1,
+    playerName: player1,
+    dicesValue: [],
+    previousValue: 0,
+    currentValue: 0,
+    readyAction: false,
+    currentCell: { cellName: FinisherCellName, cellId: 3, },
+    inHouse: false,
+    inMainCell: false,
+    inFinisherCell: true,
+    inSuccessCell: false,
+  },
+
+  {
+    id: 2,
+    playerName: player1,
+    dicesValue: [],
+    previousValue: 0,
+    currentValue: 0,
+    readyAction: false,
+    currentCell: 0,
+    inHouse: true,
+    inMainCell: false,
+    inFinisherCell: false,
+    inSuccessCell: false,
+  },
+
+  {
+    id: 3,
+    playerName: player1,
+    dicesValue: [],
+    previousValue: 0,
+    currentValue: 0,
+    readyAction: false,
+    currentCell: { cellName: MainCellName, cellId: 2 },
+    inHouse: false,
+    inMainCell: true,
+    inFinisherCell: false,
+    inSuccessCell: false,
+  },
+
+  {
+    id: 4,
+    playerName: player1,
+    dicesValue: [],
+    previousValue: 0,
+    currentValue: 0,
+    readyAction: false,
+    currentCell: { cellName: MainCellName, cellId: 2 },
+    inHouse: false,
+    inMainCell: true,
+    inFinisherCell: false,
+    inSuccessCell: false,
+  },
+]
